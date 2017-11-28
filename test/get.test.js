59adcebe186822d5ed26026a1486c35044fe36ae
wangyang <1451262987@qq.com>
2017-11-28 16:49:11 +0800

    mock数据生成å :neutral_face:

--- test/get.test.js
+++
@@ -1,9 +0,0 @@
-var get=require('../app/common/get.js');
-
-describe('multi', () => {
-    for (let i = 0; i < 10; i++) {
-        it('multi', () => {
-            expect(get.multi(i, 10)).toEqual(i * 10);
-        });
-    }
-});
