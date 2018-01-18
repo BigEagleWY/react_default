ac17f824fc348f4ebbec079064bd7a04ea0413b9
wangyang <wangyang@vmobel.com>
2018-01-18 15:35:22 +0800

    文档插入音频视频

--- app/main.js
+++ app/main.js
@@ -1,47 +1,18 @@
 //main.js
 import React from 'react';
 import {render} from 'react-dom';
-import {Provider} from 'react-redux';
 
-import {
-    Router,
-    Route,
-    Link,
-    hashHistory,
-    IndexRoute,
-    Redirect,
-    IndexLink,
-    browserHistory
-} from 'react-router';
 
 //容器 Carousel
-import Index from './containers/Index.js';
-import CssModules from './containers/CssModules.js';
-import Carousel from './containers/Carousel.js';
-import ThreeScreen from './containers/ThreeScreen.js';
-import OnePaperDoc from './containers/OnePaperDoc.js';
-import Gallery from './containers/Gallery.js';
-import Video from './containers/Video.js';
+import Image from './components/Image.jsx';
 
-import Store from './store/';
 
-const store = Store();
+
 
 
 
 render((
-    <Provider store={store}>
-        <Router history={hashHistory}>
-            <Route path="/" component={Index}></Route>
-            <Route path="/index" component={Index}></Route>
-            <Route path="/cssmodules" component={CssModules}></Route>
-            <Route path="/carousel" component={Carousel}></Route>
-            <Route path="/threescreen" component={ThreeScreen}></Route>
-            <Route path="/doc" component={OnePaperDoc}></Route>
-            <Route path="/gallery" component={Gallery}></Route>
-            <Route path="/video" component={Video}></Route>
-        </Router>
-    </Provider>
+    <Image />
 ), document.getElementById('con'));
 
 // WEBPACK FOOTER // ./app/main.js
--- app/reducers/index.js
+++
@@ -1,7 +0,0 @@
-const initState={}
-export default function getData(state=initState,action){
-    switch(action.type){
-        default:
-            return state;
-    }
-}
